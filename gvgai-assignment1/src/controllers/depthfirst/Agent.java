d5e623bb2fc1f8e7e0ef7eeb31235a854b489432
iznauy <zy05160516@126.com>
2018-05-06 23:47:25 +0800

    add the first part of assignment1

---
+++ gvgai-assignment1/src/controllers/depthfirst/Agent.java
@@ -0,0 +1,65 @@
+package controllers.depthfirst;
+
+import core.game.StateObservation;
+import core.player.AbstractPlayer;
+import ontology.Types;
+import tools.ElapsedCpuTimer;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.LinkedList;
+
+/**
+ * Created on 2018/5/4.
+ * Description:
+ *
+ * @author iznauy
+ */
+public class Agent extends AbstractPlayer {
+
+    private LinkedList<Types.ACTIONS> actions = new LinkedList<>();
+
+    private LinkedList<StateObservation> visitedObs = new LinkedList<>();
+
+    public Agent(StateObservation so, ElapsedCpuTimer elapsedTimer) {
+        dfs(so);
+    }
+
+    @Override
+    public Types.ACTIONS act(StateObservation stateObs, ElapsedCpuTimer elapsedTimer) {
+//        System.out.println(actions.size());
+//        try {
+//            Thread.sleep(500);
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+        return actions.removeFirst();
+    }
+
+    private boolean dfs(StateObservation stateObs) {
+        visitedObs.add(stateObs);
+
+        if (stateObs.isGameOver()) // game over, win or not?
+            return stateObs.getGameWinner() == Types.WINNER.PLAYER_WINS;
+
+        for (Types.ACTIONS action: stateObs.getAvailableActions()) {
+            StateObservation stateObsCopy = stateObs.copy();
+            stateObsCopy.advance(action);
+
+            if (isVisited(stateObsCopy)) continue; // has visited
+
+            actions.add(action);
+            if (dfs(stateObsCopy))
+                return true;
+
+            actions.removeLast();
+        }
+
+        return false;
+    }
+
+    private boolean isVisited(StateObservation stateObs) {
+        return visitedObs.stream().anyMatch(e -> e.equalPosition(stateObs));
+    }
+
+}
